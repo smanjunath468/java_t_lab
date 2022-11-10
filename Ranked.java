5a4ef4ebb57458a3f14dc8ed00d774e92787ccd0
smanjunath468 <116783600+smanjunath468@users.noreply.github.com>
2022-11-10 13:41:03 +0530

    ranking

---
+++ Ranked.java
@@ -0,0 +1,15 @@
+
+public class Ranked {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String names[] = { "Manjunath S ", "Suresh ", "Bhuvan " , "Kamlesh ", "Vignesh " };
+        
+        String rank[] = {"Distingtion","First class " , "Second class","pass","Fail" };
+        
+        for(int i = 0; i < names.length; i++)
+        {
+            System.out.println( names[i]   +   rank[i] + "." );
+        }    
+	}
+
+}
