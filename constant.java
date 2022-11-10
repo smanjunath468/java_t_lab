de18172bd3bf01233f2351400244250cd30d5b57
smanjunath468 <116783600+smanjunath468@users.noreply.github.com>
2022-11-10 13:25:39 +0530

    constant

---
+++ constant.java
@@ -0,0 +1,16 @@
+
+public class constant {
+	static final int integerConstant = 20;
+	   static final String stringConstant = "hello";
+	   static final float floatConstant = 1654.22f;
+	   static final char characterConstant = 'C';
+	    
+	
+	   public static void main(String args[]) {
+	      System.out.println("value of integerConstant: "+constant.integerConstant);
+	      System.out.println("value of stringConstant: "+constant.stringConstant);
+	      System.out.println("value of floatConstant: "+constant.floatConstant);
+	      System.out.println("value of characterConstant: "+constant.characterConstant);
+	   }
+
+}
