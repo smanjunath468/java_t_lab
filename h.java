c802b3c368f798becc69f5c1859c8a61788aebd4
smanjunath468 <116783600+smanjunath468@users.noreply.github.com>
2022-11-17 11:49:55 +0530

    17/11/2022
    5 String functions

---
+++ h.java
@@ -0,0 +1,23 @@
+package manjunath;
+
+public class h {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="MELFIE";
+        System.out.println(str.indexOf('s'));  
+        
+        System.out.println(str.replace('m','S'));    //replace m by S
+        
+        System.out.println(str.length());  //length of the String
+        
+        System.out.println(str.substring(4));  //it will print after 4 character
+        
+       
+        
+        System.out.println(str.toLowerCase()); //lowerCase letter
+        
+        
+	}
+
+}
